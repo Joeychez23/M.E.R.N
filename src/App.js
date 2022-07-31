c7d184c8c411721f16a2af1052c1d9e3ba925dac
Joey Sanchez <joeysanchez@joeys-mbp.lan>
2022-07-31 14:38:16 -0700

    L

--- src/App.js
+++ src/App.js
@@ -7,6 +7,26 @@ import './App.css';
 function App() {
   const [counter, setCounter] = useState(0);
 
+  getData();
+  async function getData() {
+    const response = await fetch('/books');
+    const val = await response.json();
+    console.log(val);
+
+    for(item of val) {
+        const title = document.createElement('div');
+        const author = document.createElement('div');
+        author.setAttribute("style", "margin-bottom: 20px");
+        title.textContent = `Title: ${item.Title}`;
+        author.textContent = `Author: ${item.Author}`;
+
+        data.append(title, author);
+
+
+    }
+}
+
+
   useEffect(() => {
     setCounter(100);
   }, [])
@@ -15,6 +35,7 @@ function App() {
       <button onClick={() => setCounter((prevCount) => prevCount -1)}>-</button>
       <h1>{counter}</h1>
       <button onClick={() => setCounter((prevCount) => prevCount +1)}>+</button>
+      <div className= 'data'></div>
 
     </div>
   );
