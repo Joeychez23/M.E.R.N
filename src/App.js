7b46733c894005829587990d38de232bf7019939
Joey Sanchez <joeysanchez@joeys-mbp.lan>
2022-07-31 14:56:09 -0700

    L

--- src/App.js
+++ src/App.js
@@ -3,30 +3,24 @@ import { useState, useEffect} from 'react';
 import './App.css';
 
 
-
+/*
 getData();
 async function getData() {
   const response = await fetch('/books');
   const val = await response.json();
   console.log(val);
-  /*
-
-  for(item of val) {
-      //const title = document.createElement('div');
-      //const author = document.createElement('div');
-      //author.setAttribute("style", "margin-bottom: 20px");
-      //title.textContent = `Title: ${item.Title}`;
-      //author.textContent = `Author: ${item.Author}`;
-
-      //data.append(title, author);
-    }
-    */
-}
+}*/
 
 
 
 function App() {
   const [counter, setCounter] = useState(0);
+  getData();
+  async function getData() {
+    const response = await fetch('/books');
+    const val = await response.json();
+    console.log(val);
+  }
 
 
   useEffect(() => {
