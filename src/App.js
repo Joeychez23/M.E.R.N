f3b552fe55086f84487821ae772c80fa5be32ec3
Joey Sanchez <joeysanchez@joeys-mbp.lan>
2022-08-03 15:59:14 -0700

    L

--- src/App.js
+++ src/App.js
@@ -38,9 +38,30 @@ function App() {
 
   return (
     <div className="App">
+      <nav class = "M">
+          <div class = "M-BTN-TOG" onclick="toggleNav()" ></div>
+          <ul class = "M-L">
+              <li class = "M-L-I"><a class = "M-L-I-A" href = "https://shielded-scrubland-08639.herokuapp.com/">Home</a></li>
+              <li class = "M-L-I"><a class = "M-L-I-A" href = "#">Portfolio</a></li>
+              <li class = "M-L-I"><a class = "M-L-I-A" href = "#">About Us</a></li>
+              <li class = "M-L-I"><a class = "M-L-I-A" href = "#">Gallery</a></li>
+              <li class = "M-L-I"><a class = "M-L-I-A" href = "#">Contact</a></li>
+          </ul>
+      </nav>
+
+
+      <aside class = "S">
+          <ul class = "S-L">
+              <li class = "S-L-I"><span class = "S-L-I-S">Projects</span></li>
+              <li class = "S-L-I"><a class = "S-L-I-A" href = "https://obscure-ravine-94158.herokuapp.com/">Poker Project</a></li>
+              <li class = "S-L-I"><a class = "S-L-I-A" href = "https://pure-island-00084.herokuapp.com/">Location & AQ Data</a></li>
+              <li class = "S-L-I"><a class = "S-L-I-A" href = "https://gentle-scrubland-61883.herokuapp.com/">Map Location</a></li>
+              <li class = "S-L-I"><a class = "S-L-I-A" href = "https://arcane-chamber-79603.herokuapp.com/">M.E.R.N Test</a></li>
+          </ul>
+      </aside>
       <>
         {
-          <div class='container-fluid' id='dataBox'>
+          <div /*class='container-fluid'*/>
             <h1 class='title'>Mongo, Express, React, Node (M.E.R.N) test</h1>
             {books.map(book => (
 
