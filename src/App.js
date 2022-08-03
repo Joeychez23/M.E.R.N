39719ff2899324595756285fd302fdfb29527305
Joey Sanchez <joeysanchez@joeys-mbp.lan>
2022-08-03 15:12:26 -0700

    L

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
   return (
     <div className="App">
       <>
-      <h1>Mongo, Express, React, Node (M.E.R.N) test</h1>
+      <h1>Mongo, Express, React, Node M.E.R.N test</h1>
         {
           <div class='container-fluid'>
             {books.map(book => (
